0a51b9e72ed293100c91376e6a4114fed521844c
Ian Renton <ian@onlydreaming.net>
2013-02-20 20:33:25 +0000

    Web UI refactoring.  Now split into separate versions for tablet and laptop use (tablet displays in portrait, doesn't use the "hold" action - laptop displays in landscape, does use the "hold" action).  Both displays now include sensor data.

--- web-ui/js/javascript.js
+++ web-ui/js/javascript.js
@@ -1,5 +1,5 @@
 /* Raspberry Tank Web UI JavaScript
-   Written by Ian Renton (http://ianrenton.com), July 2012
+   Written by Ian Renton (http://ianrenton.com), February 2013
    Released into the public domain without licence. */
 
 // All commands that could be sent to the vehicle.
@@ -23,12 +23,12 @@ var WEBCAM_PORT = 8080;
 // Executes on page load.
 function load() {
   createImageLayer();
+  setInterval(updateSensorData, 1000);
 }
 
 // Sets a command to either true or false by name, e.g. to go forwards use
 // set('forwards', true) and to stop going forwards, use set('forwards', false).
 function set(name, value) {
-  stop();
   command[name] = value;
   send();
   return true;
@@ -49,6 +49,14 @@ function send() {
   for (var name in command) {
     commandBits = commandBits + (command[name] ? "1" : "0");
   }
-  $.get(window.location.protocol+'//'+window.location.host + ':' + CONTROL_PORT + "?" + commandBits);
+  $.get(window.location.protocol+'//'+window.location.host + ':' + CONTROL_PORT + "?set" + commandBits);
 }
 
+// Gets the sensor data
+function updateSensorData() {
+  $.get("../sensordata.txt", "", function(data){
+    if (data != "") {
+      $('div.data').html("<h1>" + data + "</h1>");
+    }
+  }, "html");
+}
