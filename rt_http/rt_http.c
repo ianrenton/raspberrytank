652962163e53c5d1179256cb9a956bb2dcc71d46
Ian Renton <ian@ianrenton.com>
2014-04-10 20:37:43 +0100

    Fix tabs and spaces

--- rt_http/rt_http.c
+++ rt_http/rt_http.c
@@ -381,21 +381,21 @@ static int http_callback(struct mg_connection *conn) {
 void* launch_sensors() {
   printf("Starting sensor polling\n");
   while(1) {
-  	int fd;														// File description
-	  char *fileName = "/dev/i2c-0";    // Name of the port we will be using
-	  int addressSRF = 0x70;            // Address of the SRF02 shifted right one bit
-	  int addressCMPS = 0x60;           // Address of CMPS10 shifted right one bit
-	  unsigned char buf[10];            // Buffer for data being read/ written on the i2c bus
+    int fd;                           // File description
+    char *fileName = "/dev/i2c-0";    // Name of the port we will be using
+    int addressSRF = 0x70;            // Address of the SRF02 shifted right one bit
+    int addressCMPS = 0x60;           // Address of CMPS10 shifted right one bit
+    unsigned char buf[10];            // Buffer for data being read/ written on the i2c bus
     int tmpRange = 0;                 // Temp variable to store range
     int tmpBearing = 0;               // Temp variable to store bearing
     int tmpPitch = 0;                 // Temp variable to store pitch
     int tmpRoll = 0;                  // Temp variable to store roll
-    char* message;										// Char array to write an error message to
-	
-	  if ((fd = open(fileName, O_RDWR)) < 0) {					// Open port for reading and writing
-		  message = "Failed to open i2c port";
-	  }
-	
+    char* message;                    // Char array to write an error message to
+  
+    if ((fd = open(fileName, O_RDWR)) < 0) {          // Open port for reading and writing
+      message = "Failed to open i2c port";
+    }
+  
     //
     // RANGEFINDER
     //
@@ -403,30 +403,30 @@ void* launch_sensors() {
     // Initial pause for safety
     usleep(50000);
 
-	  if (ioctl(fd, I2C_SLAVE, addressSRF) < 0) {					// Set the port options and set the address of the device we wish to speak to
-		  message = "Unable to get bus access to talk to slave";
-	  }
-	
-	  buf[0] = 0;													// Commands for performing a ranging
-	  buf[1] = 81;
-	
-	  if ((write(fd, buf, 2)) != 2) {								// Write commands to the i2c port
-		  message = "Error writing to i2c slave\n";
-	  }
-	
-	  usleep(750000);												// This sleep waits for the ping to come back
-	
-	  buf[0] = 0;													// This is the register we wish to read from
-	
-	  if ((write(fd, buf, 1)) != 1) {								// Send the register to read from
-		  message = "Error writing to i2c slave\n";
-	  }
-	
-	  if (read(fd, buf, 4) != 4) {								// Read back data into buf[]
-		  message = "Unable to read from slave\n";
-	  } else {
-		  tmpRange = (buf[2] <<8) + buf[3];			// Calculate range as a word value
-	  }
+    if (ioctl(fd, I2C_SLAVE, addressSRF) < 0) {         // Set the port options and set the address of the device we wish to speak to
+      message = "Unable to get bus access to talk to slave";
+    }
+  
+    buf[0] = 0;                         // Commands for performing a ranging
+    buf[1] = 81;
+  
+    if ((write(fd, buf, 2)) != 2) {               // Write commands to the i2c port
+      message = "Error writing to i2c slave\n";
+    }
+  
+    usleep(750000);                       // This sleep waits for the ping to come back
+  
+    buf[0] = 0;                         // This is the register we wish to read from
+  
+    if ((write(fd, buf, 1)) != 1) {               // Send the register to read from
+      message = "Error writing to i2c slave\n";
+    }
+  
+    if (read(fd, buf, 4) != 4) {                // Read back data into buf[]
+      message = "Unable to read from slave\n";
+    } else {
+      tmpRange = (buf[2] <<8) + buf[3];     // Calculate range as a word value
+    }
 
     //
     // COMPASS
@@ -435,26 +435,26 @@ void* launch_sensors() {
     // Initial pause for safety
     usleep(50000);
 
-	  if (ioctl(fd, I2C_SLAVE, addressCMPS) < 0) {					// Set the port options and set the address of the device we wish to speak to
-		  message = "Unable to get bus access to talk to slave";
-	  }
-	
-	  buf[0] = 0;													// this is the register we wish to read from
-	
-	  if ((write(fd, buf, 1)) != 1) {								// Send register to read from
-		  message = "Error writing to i2c slave\n";
-	  }
-	
-  	if (read(fd, buf, 6) != 6) {								// Read back data into buf[]
-	  	message = "Unable to read from slave\n";
-  	}
-	  else {
-		  tmpBearing = ((buf[2]<<8) + buf[3]) / 10;
+    if (ioctl(fd, I2C_SLAVE, addressCMPS) < 0) {          // Set the port options and set the address of the device we wish to speak to
+      message = "Unable to get bus access to talk to slave";
+    }
+  
+    buf[0] = 0;                         // this is the register we wish to read from
+  
+    if ((write(fd, buf, 1)) != 1) {               // Send register to read from
+      message = "Error writing to i2c slave\n";
+    }
+  
+    if (read(fd, buf, 6) != 6) {                // Read back data into buf[]
+      message = "Unable to read from slave\n";
+    }
+    else {
+      tmpBearing = ((buf[2]<<8) + buf[3]) / 10;
       tmpPitch = buf[4];
       if (tmpPitch > 127) tmpPitch = tmpPitch-256;
       tmpRoll = buf[5];
       if (tmpRoll > 127) tmpRoll = tmpRoll-256;
-	  }
+    }
 
     // Debug
     printf("SENSOR POLL   Range: %d   Bearing: %d   Pitch: %d   Roll: %d \n", tmpRange, tmpBearing, tmpPitch, tmpRoll);
